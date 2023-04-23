024ad6056f541667a5eb1aa73a0cc686eddcb88c
egagofur <egagofurtriwahana@gmail.com>
2023-04-23 20:23:53 +0700

    v1.1

--- app/page.tsx
+++ app/page.tsx
@@ -32,15 +32,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500">
-          Hi, my name is Ega, I work with React Ecosystem and Frontend Engineer
-          Intern at
-          <Link
-            target="_blank"
-            href="https://dot.co.id"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            DOT Indonesia
-          </Link>
+          Hi, my name is Ega, I work with React Ecosystem
           <br />
           All of my projects are available at here
         </h2>
--- src/data/About/introduction.ts
+++ src/data/About/introduction.ts
@@ -1,5 +1,7 @@
 export const introduction: string[] = [
-  "I am a frontend engineer intern at DOT Indonesia.I have experience as a junior web developer. I have an interest in having a career as a frontend engineer (typescript). I'm a fast learner and self-taught, and I've learned a lot of new technologies in recent years on my own from the Internet.",
+  "I am a Frontend Enginner. I have experience as a web developer. I have an interest in having a career",
+  "as a frontend engineer (typescript). I'm a fast learner and self-taught, and I've learned a lot of new",
+  " technologies in recent years on my own from the Internet.",
   `I live in Malang, Indonesia. I'm ${calculateAge(
     new Date("2005-06-09")
   )} years old. I'm a Computer Science Student from Vocational School.`,
